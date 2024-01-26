b14719b8228af785a356aeb13f86553a79a3d983
Aditya Pawar <adiispawar@gmail.com>
2024-01-26 15:03:34 +0530

    add tha increment and decrment functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [count,setCount]=useState(0);
+  function decreaseHandler()
+  {
+      setCount(count-1);
+  }
+  function increaseHandler()
+  {
+      setCount(count+1);
+  }
+  function resetHandler()
+  {
+    setCount(0);
+     
+  }
+
   return (
-     <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col'>
-      <div>Increament && Decrement </div>
-      <div>
-        <button>
+     <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10'>
+      <div className='text-[#0398d4] font-medium text-2xl '>Increament & Decrement </div>
+      <div className='flex bg-white justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]' >
+        <button onClick={decreaseHandler} className='boder-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>
         -
-
         </button>
-        <div>
-
+        <div  className='font-bold gap-12 text-5xl'>
+           {count}
         </div>
-        <button>
+        <button onClick={increaseHandler} className='boder-l-2 text-center w-20 border-[#bfbfbf] text-5xl '>
           +
-
         </button>
       </div>
-      <button> Reset  </button>
+      <button onClick={resetHandler} className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg '> Reset  </button>
      </div>
   );
 }
